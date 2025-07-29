837f09413688d514453ba71a49a59e0b13d9d165
zishan044 <winchesterfelix007@gmail.com>
2025-07-29 15:58:18 +0600

    Performed exploratory data analysis and implemented data ingestion component

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from dataclasses import dataclass
+
+import pandas as pd
+
+from src.logger import logging
+from src.exception import CustomException
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    
+    def init_data_ingestion(self):
+        logging.info("Starting data ingestion process.")
+        try:
+            df = pd.read_csv('notebooks/data/sugar_prices.csv')
+            logging.info("Successfully read the raw CSV file.")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            logging.info("Ensured artifact directory exists.")
+            
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info(f"Raw data saved at: {self.ingestion_config.raw_data_path}")
+
+            train_size = int(len(df) * 0.8)
+            train_df = df.iloc[:train_size]
+            test_df = df.iloc[train_size:]
+            logging.info("Performed time-based train-test split.")
+
+            train_df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_df.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info(f"Train data saved at: {self.ingestion_config.train_data_path}")
+            logging.info(f"Test data saved at: {self.ingestion_config.test_data_path}")
+
+            logging.info("Data ingestion completed successfully.")
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            logging.error("Error occurred during data ingestion.")
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    ingestion = DataIngestion()
+    ingestion.init_data_ingestion()
